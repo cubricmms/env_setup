d9d370850708d2bbd3872bc8302246b4a4a6d393
ubuntu <ubuntu@localhost.localdomain>
2020-12-16 11:43:32 +0800

    Update with tsinghua mirrors

--- init.sh
+++ init.sh
@@ -1,11 +1,24 @@
-sudo add-apt-repository ppa:jonathonf/vim
-sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install git -y
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-    libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-    xz-utils tk-dev libffi-dev liblzma-dev -y
-curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_basic_vimrc.sh
-pyenv install 3.6.1
-apt install mongodb -y
+#!/bin/bash
+
+# vim
+cat basic.vim > ~/.vimrc
+
+# ubuntu sources
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.save
+sudo cp sources.list /etc/apt/sources.list
+sudo mv /etc/apt/sources.list.d /etc/apt/sources.list.d.save
+sudo cp -R sources.list.d /etc/apt/sources.list.d
+
+# update essentials
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y \
+	make \
+    	build-essential \
+    	vim \
+    	curl \
+    	libssl-dev \
+    	git \
+	wget \
+	python-pip \
+	python-dev \
+	python-virtualenv

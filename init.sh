f330ca4255b973c61b51c93cac3982c8a80e3bc5
cubricmms <cubric.mms@gmail.com>
2020-12-16 16:42:12 +0800

    Add node, docker-ce, nvidia-docker

--- init.sh
+++ init.sh
@@ -8,23 +8,45 @@ echo "OS: $OS, release of $RELEASE, code name: $CODENAME"
 # vim
 cat basic.vim > ~/.vimrc
 
-# ubuntu sources
+# tsinghua tuna as ubuntu source
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.save
 sudo cp sources.list /etc/apt/sources.list
 sudo sed -i -e "s/{OS}/$OS/g" -e "s/{CODENAME}/$CODENAME/g" /etc/apt/sources.list
-sudo rm -rf /etc/apt/sources.list.d.save &&  sudo mv /etc/apt/sources.list.d /etc/apt/sources.list.d.save
-sudo cp -R sources.list.d /etc/apt/sources.list.d
+
+# node 10x
+sudo cp sources.list.d/nodesource.list /etc/apt/sources.list.d/nodesource.list
+sudo sed -i -e "s/{OS}/$OS/g" -e "s/{CODENAME}/$CODENAME/g" /etc/apt/sources.list.d/nodesource.list
+
+# docker-ce
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# nivida-container
+DIST=$(. /etc/os-release; echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | sudo apt-key add -
+
+# nvidia-container-runtime
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | sudo apt-key add -
+
+# nvidia-docker
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$DIST/nvidia-docker.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
 # update essentials
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y \
 	make \
-    	build-essential \
-    	vim \
-    	curl \
-    	libssl-dev \
-    	git \
+    build-essential \
+    vim \
+    curl \
+    libssl-dev \
+    git \
 	wget \
 	python-pip \
 	python-dev \
-	python-virtualenv
+	python-virtualenv \
+    docker-ce

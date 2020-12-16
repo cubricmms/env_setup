8cbc9e9cca96d6c6ba3e3733fc52d7881616cacc
cubricmms <cubric.mms@gmail.com>
2020-12-16 13:52:34 +0800

    Dynamic sources.list

--- init.sh
+++ init.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+RELEASE=$(lsb_release -rs)
+OS="$(echo $(lsb_release -is) | tr '[A-Z]' '[a-z]')"
+CODENAME=$(lsb_release -cs)
+echo "OS: $OS, release of $RELEASE, code name: $CODENAME"
+
 # vim
 cat basic.vim > ~/.vimrc
 
 # ubuntu sources
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.save
 sudo cp sources.list /etc/apt/sources.list
-sudo mv /etc/apt/sources.list.d /etc/apt/sources.list.d.save
+sudo sed -i -e "s/{OS}/$OS/g" -e "s/{CODENAME}/$CODENAME/g" /etc/apt/sources.list
+sudo rm -rf /etc/apt/sources.list.d.save &&  sudo mv /etc/apt/sources.list.d /etc/apt/sources.list.d.save
 sudo cp -R sources.list.d /etc/apt/sources.list.d
 
 # update essentials
